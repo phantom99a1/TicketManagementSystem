ccfc589723cb2916c2dd896e12521d785ba198c8
phantom99a1 <viendanho99a1@gmail.com>
2024-11-25 17:49:23 +0700

    Update database

---
+++ Domain/Entities/Priority.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Entities
+{
+    public class Priority
+    {
+        public int PriorityId { get; set; }
+
+        public string PriorityName { get; set; } = string.Empty;
+
+        public int ExpectedDays { get; set; }
+    }
+}
